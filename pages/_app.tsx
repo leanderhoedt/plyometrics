797e4e8ce792bf4074d721505bdb833f0796839e
leanderhoedt <leander.hoedt@gmail.com>
2023-03-15 16:28:19 +0100

    Update _app.tsx
    remove logrocket-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,9 @@ import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import LogRocket from 'logrocket';
-import setupLogRocketReact from 'logrocket-react';
 
 if (typeof window !== 'undefined') {
   LogRocket.init('eoiwnx/plyometrics');
-  // plugins should also only be initialized when in the browser
-  setupLogRocketReact(LogRocket);
 }
 
 export default function App({ Component, pageProps }: AppProps) {
